d6a2373da4af7ceda9c75ae40b566c6299ae6530
Alvaro Oliveira <alvarosouza61@gmail.com>
2022-05-14 15:33:50 -0300

    Controllers configurados no GUI, adicionando e removendo processos e o select-option dos algoritmos

--- src/main.js
+++ src/main.js
@@ -3,15 +3,54 @@ import Stats from "../node_modules/three/examples/jsm/libs/stats.module.js"
 import { GUI } from '../node_modules/dat.gui/build/dat.gui.module.js'
 
 var scene, renderer, camera, axesHelper;
+var controllerProcessosAddProcesso, controllerProcessosRemoveProcesso;
+var quantidadeDeProcessos = 1;
+const gui = new GUI({name: "Escalonamento"})
+
+var processosFolder;
+
+var listaDeProcessos = []
+const LIMITE_SUPERIOR_QUANTIDADE_DE_PROCESSOS = 100;
+const LIMITE_INFERIOR_QUANTIDADE_DE_PROCESSOS = 1;
 
 function init() {
 
 	renderer = new THREE.WebGLRenderer();
 	const stats = Stats()
-	const gui = new GUI()
-	
+
 	renderer.setClearColor(new THREE.Color(0.0, 0.0, 0.0));
 	
+	processosFolder = gui.addFolder('Processos');
+	var processos = {adicionarProcesso: addProcesso, removerProcesso: removeProcesso};
+
+	controllerProcessosAddProcesso = processosFolder.add(processos, 'adicionarProcesso');
+	controllerProcessosRemoveProcesso = processosFolder.add(processos, 'removerProcesso');
+
+	processosFolder.open()
+
+	
+	var algoritmosFolder = gui.addFolder('Algoritmos de Escalonamento');
+	var algoritmosEscalonamento = {name: ''};
+	const states = [ 'FIFO', 'Round-Robin', 'EDF', 'SJF'];
+	const clipCtrl = algoritmosFolder.add( algoritmosEscalonamento, 'name' ).options( states )
+	algoritmosFolder.open()
+
+	clipCtrl.onChange( () => {
+		if(clipCtrl.object.name == "FIFO") {
+			console.log("FIFO")
+		}
+		if(clipCtrl.object.name == "Round-Robin") {
+			console.log("RR")
+		}
+		if(clipCtrl.object.name == "EDF") {
+			console.log("EDF")
+		}
+		if(clipCtrl.object.name == "SJF") {
+			console.log("SJF")
+		}
+	})
+
+
     const element = document.getElementById('canvas-three')
 	
 	element.appendChild(stats.dom)
@@ -25,9 +64,6 @@ function init() {
 	
 	scene = new THREE.Scene();
 
-	/* camera = new THREE.OrthographicCamera( width / - 2, width / 2, height / 2, height / - 2, 1, 1000 );
-	scene.add( camera ); */
-
 	camera = new THREE.OrthographicCamera( width / -100, width / 2, height / 2, height / -50, width / height, 0 );
     scene.add( camera );
 
@@ -37,6 +73,21 @@ function init() {
 
 	render()
 };
+
+function addProcesso() {
+	controllerProcessosAddProcesso.onChange( () => {
+		console.log(quantidadeDeProcessos)
+		listaDeProcessos.push(processosFolder.addFolder('Processo'+quantidadeDeProcessos));
+		quantidadeDeProcessos++;
+	})
+}
+
+function removeProcesso() {
+	controllerProcessosRemoveProcesso.onChange(() => {
+		processosFolder.removeFolder(gui.__folders.Processos.__folders['Processo'+(quantidadeDeProcessos-1)])
+		quantidadeDeProcessos--;
+	})
+}
 
 function render() {
 	renderer.render(scene, camera);
