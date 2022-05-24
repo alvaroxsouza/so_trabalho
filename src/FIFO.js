9c4d8973266c8f893441c4f54560952d47788c9f
Caio Nery <51177310+caionms@users.noreply.github.com>
2022-05-24 20:20:32 -0300

    Fifo finalizado pra integração

--- src/FIFO.js
+++ src/FIFO.js
@@ -1,43 +1,59 @@
 // Test Setup
 import { Processo } from "./Processo.js";
 
-let tempoTotal = 0;
-
 function listaDeProcessosFIFO(listaDeProcessos) {
     let tempoCorrente = 0;
 
     let listaDeRetangulos = [];
 
-    listaDeProcessos.sort(function(a, b) {
+    listaDeProcessos.sort(function (a, b) {
         if (a.tempoDeChegada > b.tempoDeChegada) { return 1; }
         if (a.tempoDeChegada < b.tempoDeChegada) { return -1; }
         return 0;
     });
 
+    if(listaDeProcessos[0].tempoDeChegada > tempoCorrente){
+        tempoCorrente += listaDeProcessos[0].tempoDeChegada;
+    }
     listaDeProcessos.forEach((processo) => {
-        if (processo.tempoDeChegada > tempoCorrente) {
+        if (processo.tempoDeChegada <= tempoCorrente) {
+            let retangulo = {
+                id: processo.id,
+                tempoInicial: tempoCorrente,
+                tempoFinal: 0
+            }
+            tempoCorrente += processo.tempoDeExecucao;
+            processo.turnAround = tempoCorrente - processo.tempoDeChegada;
+            processo.tempoDeEspera = processo.turnAround - processo.tempoDeChegada;
+            retangulo.tempoFinal = tempoCorrente;
+            listaDeRetangulos.push(retangulo)
+        }
+        else {
             tempoCorrente += processo.tempoDeChegada - tempoCorrente;
+            let retangulo = {
+                id: processo.id,
+                tempoInicial: tempoCorrente,
+                tempoFinal: 0
+            }
+            tempoCorrente += processo.tempoDeExecucao;
+            processo.turnAround = tempoCorrente - processo.tempoDeChegada;
+            processo.tempoDeEspera = processo.turnAround - processo.tempoDeChegada;
+
+            retangulo.tempoFinal = tempoCorrente;
+            listaDeRetangulos.push(retangulo)
         }
-
-        let retangulo = {
-            id: processo.id,
-            tempoInicial: tempoCorrente,
-            tempoFinal: 0
-        }
-
-        tempoCorrente += processo.tempoDeExecucao;
-        tempoTotal += tempoCorrente - processo.tempoDeChegada;
-        processo.turnAround = tempoCorrente;
-        processo.tempoDeEspera = processo.turnAround - processo.tempoDeChegada - processo.tempoDeExecucao;
-
-        retangulo.tempoFinal = tempoCorrente;
-        listaDeRetangulos.push(retangulo)
     })
     return listaDeRetangulos;
 }
 
-function findTurnAroundTime(quantidadeDeProcessos) {
-    return (tempoTotal / quantidadeDeProcessos);
+function findTurnAroundTime(listaDeProcessos) {
+    listaDeProcessosFIFO(listaDeProcessos);
+
+    let total = 0;
+    listaDeProcessos.forEach((processo) => {
+        total += processo.turnAround;
+    })
+    return (total / listaDeProcessos.length);
 }
 
 export { listaDeProcessosFIFO, findTurnAroundTime }
